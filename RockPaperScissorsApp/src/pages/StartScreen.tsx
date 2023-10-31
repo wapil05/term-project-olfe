be3db7de99481bbd53746205b32c2681fd8a79eb
FelWach <s2110238071@fhooe.at>
2023-10-31 16:05:35 +0100

    Tailwind Setup

--- RockPaperScissorsApp/src/pages/StartScreen.tsx
+++ RockPaperScissorsApp/src/pages/StartScreen.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function StartScreen() {
-  return <div>StartScreen</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Rock Paper Scissors</h1>
+      <h2>By Oliver Wolfmayr and Felix Wacha</h2>
+    </div>
+  );
 }
 
 export default StartScreen;
